a912307c7f6bd4ce9616af0905c196aa27a75df9
Thoniwut Rapeepisarn <thoniwut.r.dev@gmail.com>
2019-10-19 14:06:22 +0700

    Initial Commit

---
+++ NewsApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApps
+//
+//  Created by Tony on 19/10/2562 BE.
+//  Copyright © 2562 Teerapco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
